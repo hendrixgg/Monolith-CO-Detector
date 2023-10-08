fa6eb792f790644e363dc8afb2c54a42760c40ab
Hendrix Gryspeerdt <95045551+hendrixgg@users.noreply.github.com>
2023-10-08 12:57:24 -0400

    Got basic text showing up

--- client/App.js
+++ client/App.js
@@ -1,20 +1,24 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [coConcentration, setCoConcentration] = useState(15);
+    return (
+        <View style={styles.container}>
+            <StatusBar style="auto" />
+            <Text>MONOLITH</Text>
+            <Text>CO Concentration</Text>
+            <Text>{coConcentration} ppm</Text>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
