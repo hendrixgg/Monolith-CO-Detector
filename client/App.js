5ee35b82c0e7f2f58a28edc00fac9096e16f449b
Hendrix Gryspeerdt <95045551+hendrixgg@users.noreply.github.com>
2023-10-15 16:48:11 -0400

    basic ui skeleton complete.

--- client/App.js
+++ client/App.js
@@ -1,24 +1,22 @@
-import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import MainScreen from './components/MainScreen';
 
 export default function App() {
-    const [coConcentration, setCoConcentration] = useState(15);
+    /*
+    Data to be stored:
+    - current CO Concentration
+    - History of CO Concentration (measured once each second/minute)
+    - should have the ability to store the CO concentration on the app for even after it is closed (so that in case of emergency, ems can see on the phone, the level of CO exposure) (EXTRA)
+    - could also store location data (EXTRA)
+    */
+    const [COConcentration, setCOConcentration] = useState(NaN);
     return (
-        <View style={styles.container}>
-            <StatusBar style="auto" />
-            <Text>MONOLITH</Text>
-            <Text>CO Concentration</Text>
-            <Text>{coConcentration} ppm</Text>
-        </View>
+        <SafeAreaProvider>
+            <MainScreen COConcentration={COConcentration} />
+        </SafeAreaProvider>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
